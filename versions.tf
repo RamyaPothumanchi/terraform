89021dd06c430d6b816342a75d1e5468c627ea8c
Ramya Pothumanchi <ramya641555@gmail.com>
2026-07-13 14:26:02 +0530

    chore: automated platform core and module upgrades

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   # Fixes the minimum required Terraform CLI version
-  required_version = "~> 1.3.0"
+  required_version = "~> 1.15.0"
 
   # Declares and locks provider versions
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0" # Allows minor updates (e.g., 5.x) but blocks major updates (6.0)
+      source = "hashicorp/aws"
+      version = "~> 5.40.0" # Allows minor updates (e.g., 5.x) but blocks major updates (6.0)
     }
     
   }
